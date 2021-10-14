057b95adfbd93644599cb8c0e7ced632f50bb631
Lucas Bohórquez <lucas.1701716343@ucaldas.edu.co>
2021-10-14 00:40:19 -0500

    first commit

---
+++ PrimeWorkShop/Form1.cs
@@ -0,0 +1,54 @@
+﻿using PrimeWorkShop.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeWorkShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long value = long.Parse(numericUpDown1.Text);
+            int numberOfThreads = Int32.Parse(numericUpDown2.Text);
+            MyTask t = new MyTask(numberOfThreads, value);
+
+            t.ExecuteThreads();
+
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                textBox2.AppendText("Thread N° " + (i + 1) + ": " + t.GetMessages(i));
+                textBox2.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 1;
+            numericUpDown2.Value = 1;
+            textBox2.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PrimeWorkShop/Models/MyTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeWorkShop.Models
+{
+    public class MyTask
+    {
+        public int NumberOfThreads { get; set; }
+        public long NumberToEvaluate { get; set; }
+        public static List<string> PrimeMessages = new List<string>();
+
+        public MyTask(int NT, long NE)
+        {
+            NumberOfThreads = NT;
+            NumberToEvaluate = NE;
+        }
+        public static void IsPrime(long num)
+        {
+            bool isPrime = false;
+            long countDiv = 2;
+            long limit = (long)(Math.Sqrt(num));
+
+            if (num > 1)
+            {
+                if (num % 2 != 0)
+                {
+                    for (long k = 3; k <= limit && countDiv < 3; k += 2)
+                    {
+                        if (num % k == 0)
+                        {
+                            countDiv++;
+                        }
+                    }
+                }
+                else if (num != 2)
+                {
+                    countDiv = 3;
+                }
+
+                isPrime = (countDiv == 2);
+            }
+            string message = "The number -" + num + "-" + "it\'s "; 
+            message += isPrime ? "Prime" : "Not Prime";
+
+            lock (PrimeMessages)
+            {
+                PrimeMessages.Add(message);
+            }
+
+        }
+
+        public void ExecuteThreads()
+        {
+            Thread[] theThreads = new Thread[NumberOfThreads];
+
+            for (int i = 0; i < NumberOfThreads; i++)
+            {
+                theThreads[i] = new Thread(() => IsPrime(NumberToEvaluate));
+                theThreads[i].Name = "Thread " + i;
+            }
+
+            foreach (var thread in theThreads)
+            {
+                thread.Start();
+                thread.Join();
+            }
+        }
+
+        public string GetMessages(int position)
+        {
+            string message = "";
+
+            if(PrimeMessages.Count > 0)
+            {
+                message = PrimeMessages[position];
+            }
+            return message;
+        }
+    }
+}
